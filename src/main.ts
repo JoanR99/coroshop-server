aba13d6adfb0d117f2c35461d26999c1ddc98f9e
Joan Romero <romerojoan1999@gmail.com>
2022-11-08 19:41:11 -0400

    add dynamic port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions as any);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
