6b4a87ff377cab5f48d920cfc92bd009123eba3e
Joan Romero <romerojoan1999@gmail.com>
2022-11-08 20:40:40 -0400

    all done

--- src/corsOptions.ts
+++ src/corsOptions.ts
@@ -5,7 +5,6 @@ import allowedOrigins from './allowedOrigins';
 const corsOptions: CorsOptions = {
   origin: (origin: string, callback) => {
     if (origin && allowedOrigins.indexOf(origin) !== -1) {
-      console.log(origin);
       callback(null, origin);
     } else {
       callback(new Error('Not allowed by CORS'));
