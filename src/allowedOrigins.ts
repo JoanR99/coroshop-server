03fd39fb9887cfdc23a1ed02a0cf33f0bea6af07
Joan Romero <romerojoan1999@gmail.com>
2022-11-08 20:25:49 -0400

    working to resolve cors problems

--- src/allowedOrigins.ts
+++ src/allowedOrigins.ts
@@ -1,7 +1,7 @@
 const allowedOrigins: string[] = [
-  'https://coroshop-client.vercel.app/',
-  'https://coroshop-client-romerojoan1999-gmailcom.vercel.app/',
-  'https://coroshop-client-git-main-romerojoan1999-gmailcom.vercel.app/',
+  'https://coroshop-client.vercel.app',
+  'https://coroshop-client-romerojoan1999-gmailcom.vercel.app',
+  'https://coroshop-client-git-main-romerojoan1999-gmailcom.vercel.app',
   'http://localhost:5173',
   'https://studio.apollographql.com',
 ];
