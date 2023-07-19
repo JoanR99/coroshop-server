7c9bf6c940b24363a7083ebdb71d1a2e9b223419
Joan Romero <romerojoan1999@gmail.com>
2023-07-19 15:17:48 -0400

    resolve cors error

--- src/corsOptions.ts
+++ src/corsOptions.ts
@@ -4,10 +4,7 @@ import allowedOrigins from './allowedOrigins';
 
 const corsOptions: CorsOptions = {
   origin: (origin: string, callback) => {
-    if (
-      (origin && allowedOrigins.indexOf(origin) !== -1) ||
-      (process.env.NODE_ENV === 'development' && typeof origin == 'undefined')
-    ) {
+    if (origin && allowedOrigins.indexOf(origin) !== -1) {
       callback(null, origin);
     } else {
       callback(new Error('Not allowed by CORS'));
