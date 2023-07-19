e630faf077f8c8cc46de0c5dbf8908519c1412e6
Joan Romero <romerojoan1999@gmail.com>
2023-07-19 15:35:22 -0400

    remove logs to origin

--- src/corsOptions.ts
+++ src/corsOptions.ts
@@ -4,7 +4,6 @@ import allowedOrigins from './allowedOrigins';
 
 const corsOptions: CorsOptions = {
   origin: (origin: string, callback) => {
-    console.log('origin', origin);
     if (origin && allowedOrigins.indexOf(origin) !== -1) {
       callback(null, origin);
     } else {
